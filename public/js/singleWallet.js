a15e10b9f93647a08e103977bbd897d181072cef
Byadiso <byadiso@gmail.com>
2021-09-11 13:33:00 +0200

    debuggin-single-money

--- public/js/singleWallet.js
+++ public/js/singleWallet.js
@@ -15,11 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let Mypro = pro.find((item) => () => {
         item.money.id[0] === proId
     })
-
+   
     let newMoneys = Mypro.moneys;
+    newMoneys=[...newMoneys]
       
-    let findedOne = newMoneys.find((item) => item._id === proId);
-    console.log(findedOne) 
+    let findedOne = newMoneys.find((item) =>()=> item._id === proId);
+    console.log(newMoneys) 
+    console.log(proId)
+    console.log(findedOne)
     let { description,amount,title,category, _id } = findedOne;
 
  
@@ -28,16 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const renderPro = () => {       
         const moneyContainer = document.createElement('DIV');       
-        let photoUrl = `http://localhost:3000/api/v1/money/photo/${proId}`;     
-       let isIncart = checkInCart(_id);
-
-           
+         
+                
         
 
     moneyContainer.innerHTML =`
-                <div class="money_container" data-toAdd="${_id}">
-                    <img src=${photoUrl} class="imgCreated" style="width: 350px; height: 400px;">
-                    <button class="btn_addCart"><i class="fas fa-shopping-cart"></i>${isIncart ? "In Cart": "Add to cart"}</button>
+                <div class="money_container" data-toAdd="${_id}">                   
+                   <h4>${description}</h4>
                 </div>
                  `                  
                 
@@ -45,200 +45,27 @@ document.addEventListener('DOMContentLoaded', () => {
         //appending the main container
         mainSingleDiv.appendChild(moneyContainer);       
 
-        //  trying to addToCart butto a even listenenre
-         const btns = document.querySelectorAll('.btn_addCart');         
-         btns.forEach((btn)=>{
-              //disable add to cart button
-              isIncart ?  btn.setAttribute('disabled', false) : btn.removeAttribute('disabled');          
-
-              // add event listener
-             btn.addEventListener('click', (event)=>{  
-                let item_id = event.target.parentNode.dataset.toAdd;
-                let buttonAddToCart =  event.target;               
-                addToCart(item_id, buttonAddToCart );                    
-             });
-         });
-      
-        
-
-
-    function addToCart(item_id, buttonContent){                
-            let cart=[];     
-            let moneysItem = {...JSON.parse(localStorage.getItem('moneys'))};           
-            let newMoneys = moneysItem.moneys;            
-            let itemTobeAdded = newMoneys.find((item) => item._id === item_id);           
-                        
-   
-            if (typeof window !== 'undefined') {
-                if (localStorage.getItem('cart')) {
-                    cart = JSON.parse(localStorage.getItem('cart'));
-                }
-                cart.push({
-                    ...itemTobeAdded,
-                    count: 1
-                });  
-        
-                // remove duplicates
-                // build an Array from new Set and turn it back into array using Array.from
-                // so that later we can re-map it
-                // new set will only allow unique values in it
-                // so pass the ids of each object/product
-                // If the loop tries to add the same value again, it'll get ignored
-                // ...with the array of ids we got on when first map() was used
-                // run map() on it again and return the actual product from the cart
-        
-                cart = Array.from(new Set(cart.map(p => p._id))).map(id => {
-                    return cart.find(p => p._id === id);
-                });
-             
-                localStorage.setItem('cart', JSON.stringify(cart));                             
-                buttonContent.innerHTML=`<i class="fas fa-shopping-cart"></i>In cart`;
-               
-               
-                const cart_items_container = document.querySelectorAll(".cart-items");
-
-                cart_items_container.forEach((cart_item_number)=>{
-                    cart_item_number.textContent = cart.length + cart_items
-                });                                             
-          }
-        }
-        
-
-
-         //check in cartlogic
-    function checkInCart(id) {
-             let stateInCart = false ;
-            if (typeof window !== 'undefined') {
-                if (localStorage.getItem('cart')) {
-                  let cartIn = [...JSON.parse(localStorage.getItem('cart'))];
-                   
-                  let cartArray = []
-                   cartArray = [...cartArray,cartIn]
-                 
-                 let newCart = cartArray[0]
-                 newCart.forEach((item)=>{
-                       if(item._id === id){
-                        stateInCart = true;
-                        return stateInCart
-                       }
-                       return ;                   
-                   })                      
-    
-                }
-            }
-
-            return stateInCart
-
-        }    
     }
-
+   
     //  fetchingSingle();
 
     renderPro()
-            
+           
 
 
  //for acessing my comments reviews and description
  const descriptionCont = document.querySelector('.description_details')
- const ShippingCont = document.querySelector('.shipping_details')
- const reviewsCont = document.querySelector('.reviews_details')
- const commentsCont = document.querySelector('#comment_details_container')
 
  // set all content
- descriptionCont.textContent = description
- ShippingCont.textContent =
-     shipping == true
-         ? 'Please remember to update your address in order to deliver you product at your door'
-         : 'Sorry this is not delivable product'
- reviewsCont.textContent =
-     reviews.length == 0
-         ? 'No Reviews found!'
-         : reviews.map((review) => {
-               const reviewsContainer = document.createElement('div')
-               reviewsContainer.innerHTML = ` <p>${review}</p>
- <p>${review.postedBy.name}</p>
- `
-           })
-
- // ...........................comment section......................................................
- const comment_details_header = document.querySelector(
-     '.comment_details_header'
- )
- comment_details_header.textContent = comments.length
-
- // condition to for rendering a text if no comments and if there are comments show them
-
- if (comments.length == 0) {
-     return 'No comments to dislay'
- } else {
-     renderComments()
- }
-
- // render my comments
- function renderComments() {
-     for (var i = 0; i < comments.length; i++) {
-         let user_loggin  = { ...JSON.parse(localStorage.getItem('user')) }        
-         const { text, _id, createdBy, created } = comments[i]
-         const comentContainer = document.createElement('div')
-         comentContainer.innerHTML = `        
-        <div class="comment_details" data-comment="${text}">
-             <p class="comment" >${text}</p>
-             <p class="posted_by">Posted by ${ createdBy.name }</p>   
-             <p class="date_posted">Comented on ${ created }</p>
-        </div>
-        
-         ${user_loggin.user._id == createdBy._id ? `<button class="btn-delete">delete</button>` : ''}
-        <hr />`
-        commentsCont.append(comentContainer)
-     }
-
-     
+ descriptionCont.textContent = description 
+ 
+         
    // access user and token
     const user= JSON.parse(localStorage.getItem('user'));
     const userId = user.user._id;
     const token = user.token; 
     let moneyId =_id
              
-
-
-     //uncomment 
-     const deleteComment_btn = document.querySelectorAll('.btn-delete');
-
-     deleteComment_btn.forEach(Btn => {    
-            Btn.addEventListener('click', (e)=>{
-                        // const comments = document.querySelector('.comment')[0];
-                        let comment_created = e.target.parentNode.children[0].dataset.comment
-                        console.log(comment_created)
-                        return fetch(`http://localhost:3000/api/v1/money/uncomment/`, {
-                            method: "PUT",
-                            headers: {
-                                Accept: "application/json",
-                                "Content-Type": "application/json",
-                                Authorization: `Bearer ${token}`
-                            },
-                            body: JSON.stringify( { userId , moneyId, comment: {text: comment_created} })
-                        })
-                .then(data => {
-                    // console.log(data)
-                    if(data.status == true){
-                                          
-                       success_message.innerHTML = `<h3>Your comment has been successfully removed</h3>`
-                      //  location.reload();
-                      
-                    } 
-                    if(data.status == false){
-                      console.log(data.error)
-                      display_error.innerHTML = data.error
-                    }         
-                  })
-                .catch(err => console.log(err));  
-               
-                 })
- 
-               });
-
-    }
-    
 
     // ----------------------------------------------------------------------------------
 
@@ -283,8 +110,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchingRelated();
 
 
-
-
 // add moneys in cart 
 document.body.addEventListener( 'click', function ( event ) { 
 
@@ -314,29 +139,6 @@ function addToCart(item_id, buttonContent){
     console.log(cart);  
     
 }
-
-
-
-//  //check money in cart
-//  function checkIfInCart (id){
-//     let storedInCarrt = JSON.parse(localStorage.getItem('cart'))
-//     let cart = document.querySelectorAll(".cart-items")[0];
-//     if(){
-
-//     }
-//     cart.textContent = storedInCarrt.length;
-
-//     let isInCart= storedInCarrt.find(item =>item._id == id);
-//     if(isInCart){
-        
-//         let button = document.querySelector('.btn_addCart');
-//         button.innerHTML=`<i class="fas fa-shopping-cart"></i> In cart`;
-//         button.setAttribute('disabled',true);
-//  }
-// }
-
-
-
 
 
             // time display in readable format
